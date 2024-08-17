6cbc94a9cc96bed08eac352cde4adb6f892a292b
Vaishnavi <31287487+vaishnavi98@users.noreply.github.com>
2024-08-17 20:51:48 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,4 +1,3 @@
-# Uncomment the imports below before you add the function code
 import requests
 import os
 from dotenv import load_dotenv
@@ -6,31 +5,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url', default="http://localhost:3030"
+)
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default = "http://localhost:5050/")
-
+    'sentiment_analyzer_url', default="http://localhost:5050/"
+)
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if (kwargs):
+    if kwargs:
         for key, value in kwargs.items():
-            params = params + key + "=" + value + "&" 
-    request_url = backend_url + endpoint + "?" + params
+            params += f"{key}={value}&"
+    request_url = f"{backend_url}{endpoint}?{params}"
 
-    print("GET from {} ".format(request_url))
+    print(f"GET from {request_url}")
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
     except Exception as e:
-        print(f"Network exception occurred {e}")
+        print(f"Network exception occurred: {e}")
 
-
-# Add code for retrieving sentiments
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url + "analyze/" + text
+    request_url = f"{sentiment_analyzer_url}analyze/{text}"
     try:
         response = requests.get(request_url)
         return response.json()
@@ -38,13 +34,11 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
-
-# def post_review(data_dict):
 def post_review(data_dict):
-    request_url = backend_url + "/insert_review"
+    request_url = f"{backend_url}/insert_review"
     try:
-        response = requests.post(request_url, json = data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except Exception as e:
-        print(f"Network exception occurred {e}")
+        print(f"Network exception occurred: {e}")
