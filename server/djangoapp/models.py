ae7ea3d2a31f60f7e85f1be71024bbe42f086a49
Vaishnavi <31287487+vaishnavi98@users.noreply.github.com>
2024-08-17 20:39:12 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -38,10 +38,10 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-    validators = [MaxValueValidator(2023), MinValueValidator(2015)])
-    # Other fields as needed
+    type = models.CharField(max_length = 10, choices = CAR_TYPES, default = 'SUV')
+    year = models.IntegerField(default = 2023, validators = [MaxValueValidator(2023), 
+                                                           MinValueValidator(2015)])
 
+    
     def __str__(self):
         return self.name  # Return the name as the string representation
